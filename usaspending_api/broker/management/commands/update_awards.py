498fe08e49aeedd9f736a3b4a8a6b968e7b8a8a0
Nipun Monga <nipunmnga@gmail.com>
2018-02-23 19:01:17 -0500

    updating update awards to operate on matviews to recalculate totals after duplicate transactions were removed

--- usaspending_api/broker/management/commands/update_awards.py
+++ usaspending_api/broker/management/commands/update_awards.py
@@ -4,7 +4,7 @@ import timeit
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from usaspending_api.awards.models import TransactionNormalized, TransactionFPDS
+from django.db import connection
 from usaspending_api.etl.award_helpers import update_awards, update_contract_awards, update_award_categories
 
 # start = timeit.default_timer()
@@ -20,61 +20,42 @@ exception_logger = logging.getLogger("exceptions")
 class Command(BaseCommand):
     help = "Updates awards based on transactions in the database or based on Award IDs passed in"
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--fiscal_year',
-            dest="fiscal_year",
-            nargs='+',
-            type=int,
-            help="Year for which to run the historical load"
-        )
-
-        parser.add_argument(
-            '--all',
-            action='store_true',
-            dest='all',
-            default=False,
-            help='Runs the award updates on all records'
-        )
-
     @transaction.atomic
     def handle(self, *args, **options):
         logger.info('Starting updates to award data...')
 
-        all_records_flag = options.get('all')
-        fiscal_year = options.get('fiscal_year')
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT id FROM fpds_dup_award_ids")
+            contract_award_ids = cursor.fetchall()
+            cursor.execute("SELECT id from fabs_dup_award_ids")
+            assistance_award_ids = cursor.fetchall()
 
-        award_update_id_list = []
-        award_contract_update_id_list = []
+            award_update_id_list = assistance_award_ids + contract_award_ids
+            award_contract_update_id_list = contract_award_ids
 
-        if not all_records_flag:
-            if fiscal_year:
-                fiscal_year = fiscal_year[0]
-                logger.info('Processing data for Fiscal Year ' + str(fiscal_year))
-            else:
-                fiscal_year = 2017
+        logger.info('Number of contract awards: %s' % str(len(contract_award_ids)))
+        logger.info('Number of assistance awards: %s' % str(len(assistance_award_ids)))
+        logger.info('Updating %s awards' % str(len(award_update_id_list)))
+        logger.info('Updating %s contract awards' % str(len(award_contract_update_id_list)))
 
-            # Lists to store for update_awards and update_contract_awards
-            award_update_id_list = TransactionNormalized.objects.filter(action_date__fy=fiscal_year).\
-                values_list('award_id', flat=True)
-            award_contract_update_id_list = TransactionFPDS.objects.filter(action_date__fy=fiscal_year).\
-                values_list('transaction__award_id', flat=True)
+        award_update_id_list = [int(award_id[0]) for award_id in award_update_id_list]
+        award_contract_update_id_list = [int(award_id[0]) for award_id in award_contract_update_id_list]
 
         logger.info('Updating awards to reflect their latest associated transaction info...')
         start = timeit.default_timer()
-        update_awards() if all_records_flag else update_awards(tuple(award_update_id_list))
+        update_awards(tuple(award_update_id_list))
         end = timeit.default_timer()
         logger.info('Finished updating awards in ' + str(end - start) + ' seconds')
 
         logger.info('Updating contract-specific awards to reflect their latest transaction info...')
         start = timeit.default_timer()
-        update_contract_awards() if all_records_flag else update_contract_awards(tuple(award_contract_update_id_list))
+        update_contract_awards(tuple(award_contract_update_id_list))
         end = timeit.default_timer()
         logger.info('Finished updating contract specific awards in ' + str(end - start) + ' seconds')
 
         logger.info('Updating award category variables...')
         start = timeit.default_timer()
-        update_award_categories() if all_records_flag else update_award_categories(tuple(award_update_id_list))
+        update_award_categories(tuple(award_update_id_list))
         end = timeit.default_timer()
         logger.info('Finished updating award category variables in ' + str(end - start) + ' seconds')
 
