3b8efefddc8c1fc47b9904dc47047f3694f36fb8
TheRJB <bodine_russell@bah.com>
2018-03-09 16:24:53 -0500

    remove contracts and add historic select mat view

--- usaspending_api/broker/management/commands/update_awards.py
+++ usaspending_api/broker/management/commands/update_awards.py
@@ -25,21 +25,23 @@ class Command(BaseCommand):
         logger.info('Starting updates to award data...')
 
         with connection.cursor() as cursor:
-            cursor.execute("SELECT id FROM fpds_dup_award_ids")
-            contract_award_ids = cursor.fetchall()
+            # cursor.execute("SELECT id FROM fpds_dup_award_ids")
+            # contract_award_ids = cursor.fetchall()
+            cursor.execute("CREATE MATERIALIZED VIEW fabs_dup_award_ids AS (SELECT DISTINCT id from awards as aw where aw.certified_date != (select action_date from transaction_normalized as txn where txn.id = aw.latest_transaction_id));")
+            logger.info('Created matview')
             cursor.execute("SELECT id from fabs_dup_award_ids")
             assistance_award_ids = cursor.fetchall()
 
-            award_update_id_list = assistance_award_ids + contract_award_ids
-            award_contract_update_id_list = contract_award_ids
+            award_update_id_list = assistance_award_ids  # + contract_award_ids
+            # award_contract_update_id_list = contract_award_ids
 
-        logger.info('Number of contract awards: %s' % str(len(contract_award_ids)))
+        # logger.info('Number of contract awards: %s' % str(len(contract_award_ids)))
         logger.info('Number of assistance awards: %s' % str(len(assistance_award_ids)))
-        logger.info('Updating %s awards' % str(len(award_update_id_list)))
-        logger.info('Updating %s contract awards' % str(len(award_contract_update_id_list)))
+        # logger.info('Updating %s awards' % str(len(award_update_id_list)))
+        # logger.info('Updating %s contract awards' % str(len(award_contract_update_id_list)))
 
         award_update_id_list = [int(award_id[0]) for award_id in award_update_id_list]
-        award_contract_update_id_list = [int(award_id[0]) for award_id in award_contract_update_id_list]
+        # award_contract_update_id_list = [int(award_id[0]) for award_id in award_contract_update_id_list]
 
         logger.info('Updating awards to reflect their latest associated transaction info...')
         start = timeit.default_timer()
@@ -47,11 +49,11 @@ class Command(BaseCommand):
         end = timeit.default_timer()
         logger.info('Finished updating awards in ' + str(end - start) + ' seconds')
 
-        logger.info('Updating contract-specific awards to reflect their latest transaction info...')
-        start = timeit.default_timer()
-        update_contract_awards(tuple(award_contract_update_id_list))
-        end = timeit.default_timer()
-        logger.info('Finished updating contract specific awards in ' + str(end - start) + ' seconds')
+        # logger.info('Updating contract-specific awards to reflect their latest transaction info...')
+        # start = timeit.default_timer()
+        # update_contract_awards(tuple(award_contract_update_id_list))
+        # end = timeit.default_timer()
+        # logger.info('Finished updating contract specific awards in ' + str(end - start) + ' seconds')
 
         logger.info('Updating award category variables...')
         start = timeit.default_timer()
